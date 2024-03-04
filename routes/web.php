2d20b497b4cada5cf9752e4ec5a73902145574d5
Tyase Nisa'an Jamilaa <tyasenj@gmail.com>
2024-03-05 04:16:31 +0700

    Perubahan Jobsheet 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LevelController;
+//use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +19,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
+//Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
