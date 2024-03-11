f5ec0ff923402156fb6210290509f6ee2c15b5a6
Tyase Nisa'an Jamilaa <tyasenj@gmail.com>
2024-03-11 22:23:06 +0700

    Perubahan Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,155 +10,52 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelangan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
 
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
 
-
-        // $data = [
-        //     'nama' => "Pelanggan Pertama",
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data);
-
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-
-        // $data = [
-        //     'level_id' => '2',
-        //     'username' => 'manager14',
-        //     'nama' => 'Manager 14',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data);
-
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::find(1);
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::where('level_id', 1)->first();
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::firstWhere('level_id', 1);
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
-        //     abort(404);
-        // });
-        // return view('user', ['data' => $user]);
-
-        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
-        //     abort(404);
-        // });
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::findOrFail(1);
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::where('username', 'manager9')->firstOrFail();
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::where('level_id', 2)->count();
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::firstOrCreate(
-        //     [
-        //         'username' => 'manager',
-        //         'nama' => 'Manager',
-        //     ]
-        // );
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::firstOrCreate(
-        //     [
-        //         'username' => 'manager22',
-        //         'nama' => 'Manager Dua Dua',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2
-        //     ]
-        // );
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::firstOrNew(
-        //     [
-        //         'username' => 'manager',
-        //         'nama' => 'Manager',
-        //     ],
-        // );
-        // return view('user', ['data' => $user]);
-
-        // $user = UserModel::firstOrNew(
-        //     [
-        //         'username' => 'manager33',
-        //         'nama' => 'Manager Tiga Tiga',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2
-        //     ],
-        // );
-        // $user->save();
-        // return view('user', ['data' => $user]);
-
-
-        // $user = UserModel::create([
-        //     'username' => 'manager55',
-        //     'nama' => 'Manager55',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 2
-        // ]);
-
-        // $user->username = 'manager55';
-
-        // $user->isDirty();
-        // $user->isDirty('username');
-        // $user->isDirty('nama'); //false
-        // $user->isDirty('nama', 'username');
-
-        // $user->isClean();
-        // $user->isDirty('username');
-        // $user->isDirty('nama'); //true
-        // $user->isDirty('nama', 'username');
-
-        // $user->save();
-
-        // $user->isDirty(); //false
-        // $user->isClean(); //true
-        // dd($user->isDirty());
-
-
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id
         ]);
 
-        $user->username = 'manager12';
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('request->password');
+        $user->level_id = $request->level_id;
 
         $user->save();
 
-        $user->wasChanged();
-        $user->wasChanged('username');
-        $user->wasChanged('username', 'level_id');
-        $user->wasChanged('nama'); //false
-        dd($user->wasChanged('nama', 'username'));
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,13 +12,17 @@
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
+                <td>Aksi</td>
             </tr>
+            @foreach ($data as $d)
             <tr>
-                <td>{{$data->user_id}}</td>
-                <td>{{$data->username}}</td>
-                <td>{{$data->nama}}</td>
-                <td>{{$data->level_id}}</td>
+                <td>{{$d->user_id}}</td>
+                <td>{{$d->username}}</td>
+                <td>{{$d->nama}}</td>
+                <td>{{$d->level_id}}</td>
+                <td><a href="/PWL_POS/public/user/ubah/{{$d->user_id}}">Ubah</a> <a href="/PWL_POS/public/user/hapus/{{$d->user_id}}">Hapus</a></td>
             </tr>
+            @endforeach
         </table>
     </body>
 </html>
