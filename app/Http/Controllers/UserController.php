db27cef807645a349f9c52f08328f1ab066708a9
Tyase Nisa'an Jamilaa <tyasenj@gmail.com>
2024-03-11 13:36:22 +0700

    Perubahan Jobsheet 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,7 +70,11 @@ class UserController extends Controller
         // return view('user', ['data' => $user]);
 
 
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // return view('user', ['data' => $user]);
+
+
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
     }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,24 +1,17 @@
 <!DOCTYPE html>
-
-<html>
-    <head>
-        <title>Data User</title>
-    </head>
-    <body>
-        <h1>Data User</h1>
-        <table border="1" cellpadding="2" cellspacing="0">
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Nama</th>
-                <th>ID Level Pengguna</th>
-            </tr>
-            <tr>
-                <td>{{$data->user_id}}</td>
-                <td>{{$data->username}}</td>
-                <td>{{$data->nama}}</td>
-                <td>{{$data->level_id}}</td>
-            </tr>
-        </table>
-    </body>
+<head>
+    <title>Data User</title>
+</head>
+<body>
+    <h1>Data User</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <tr>
+            <th>Jumlah Pengguna:</th>
+        </tr>
+        <tr>
+            <th>{{ $data }}</th>
+        </tr>
+</body>
 </html>
+
+
