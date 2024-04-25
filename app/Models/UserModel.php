7f150b2017cbb5bc6ccb24712e70e1d8ca4842ab
Tyase Nisa'an Jamilaa <tyasenj@gmail.com>
2024-04-25 11:42:03 +0700

    Jobsheet 10: Auth - Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,16 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class UserModel extends User
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
 
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
     /**
      * @var array
      */
